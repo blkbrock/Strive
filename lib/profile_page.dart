77e2c2bb7118a07064db6ddbc44b9884b9ea5f47
Brock Krusemark <kruse331@morris.umn.edu>
2023-02-05 04:02:27 -0600

    profile_page overhaul - each User has identically rendered profile page, but user id is used as key for getting user data

--- lib/profile_page.dart
+++ lib/profile_page.dart
@@ -1,8 +1,15 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:strive/main.dart';
 
+final databaseUserRef = FirebaseFirestore.instance.collection('Users');
+final databaseMsgRef = FirebaseFirestore.instance.collection('Msg');
+String profileID = '';
+
 class ProfilePage extends StatefulWidget {
-  const ProfilePage({super.key});
+  ProfilePage(String id, {super.key}) {
+    profileID = id;
+  }
 
   @override
   State<ProfilePage> createState() => _ProfilePage();
@@ -10,6 +17,46 @@ class ProfilePage extends StatefulWidget {
 
 class _ProfilePage extends State<ProfilePage> {
   void showHistory() {}
+  String user = '', msg = '',sendID='',sendMsg='';
+
+  Future<DocumentSnapshot> retrieveProfileData(String id) async {
+    return databaseUserRef.doc(id).get();
+  }
+
+  Future<DocumentSnapshot> retrieveMsgData(String id) async {
+    return databaseMsgRef.doc(id).get();
+  }
+
+  Future<void> getUserData() async {
+    DocumentSnapshot profileData = await retrieveProfileData(profileID);
+    if (profileData.data() != null) {
+      user = profileData.data().toString();
+    }
+    if (profileData.data() == null) {
+      user = '';
+    }
+  }
+
+  Future<void> getUserMsg() async {
+    DocumentSnapshot msgData = await retrieveMsgData(profileID);
+    if (msgData.data() != null) {
+      msg = msgData.data().toString();
+    }
+    if (msgData.data() == null) {
+      msg = '';
+    }
+  }
+
+  void sendMessage(String id, String msg) {
+    databaseMsgRef.doc(id).set({"Msg": msg});
+  }
+
+  _ProfilePage() {
+    getUserData();
+    Future.delayed(const Duration(milliseconds: 2000), () {
+      setState(() {});
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,11 +66,13 @@ class _ProfilePage extends State<ProfilePage> {
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-            const Text(
-              'Name',
-            ),
+            const SizedBox(height: 50),
+            Text(user,
+                style: const TextStyle(
+                    color: Colors.deepPurpleAccent, fontSize: 24)),
+            const SizedBox(height: 100),
             ElevatedButton(
                 onPressed: () {
                   showDialog(
@@ -40,7 +89,7 @@ class _ProfilePage extends State<ProfilePage> {
                                 children: const <Widget>[
                                   ColoredBox(
                                       color: Colors.white10,
-                                      child: Text('| will show weight history |')),
+                                      child: Text('OVER 9000')),
                                 ],
                               ),
                             ),
@@ -49,7 +98,8 @@ class _ProfilePage extends State<ProfilePage> {
                             ElevatedButton(
                                 child: const Text("Close"),
                                 onPressed: () {
-                                  Navigator.of(context, rootNavigator: true).pop('dialog');
+                                  Navigator.of(context, rootNavigator: true)
+                                      .pop('dialog');
                                   setState(() {});
                                 })
                           ],
@@ -57,19 +107,124 @@ class _ProfilePage extends State<ProfilePage> {
                       });
                 },
                 child: const Text('Weight History')),
-            ColoredBox(
-                color: Colors.deepPurple.shade200, child: const Text('test')),
+            const SizedBox(height: 50),
+            ElevatedButton(
+                onPressed: () {
+                  showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          scrollable: true,
+                          title: const Text('Workout History'),
+                          content: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Form(
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: const <Widget>[
+                                  ColoredBox(
+                                      color: Colors.white10,
+                                      child: Text(
+                                          "'I twerked for 4hrs straight!' - Bork")),
+                                ],
+                              ),
+                            ),
+                          ),
+                          actions: [
+                            ElevatedButton(
+                                child: const Text("Close"),
+                                onPressed: () {
+                                  Navigator.of(context, rootNavigator: true)
+                                      .pop('dialog');
+                                  setState(() {});
+                                })
+                          ],
+                        );
+                      });
+                },
+                child: const Text('Workout History')),
+            const SizedBox(height: 100),
+            const Text(
+              'Messages:',
+              style: TextStyle(color: Colors.deepPurpleAccent, fontSize: 16),
+            ),
+            ElevatedButton(
+                child: const Text("refresh", style: TextStyle(fontSize: 12)),
+                onPressed: () {
+                  getUserMsg();
+                  Future.delayed(const Duration(milliseconds: 1000), () {
+                    setState(() {});
+                  });
+                }),
+            const SizedBox(height: 20),
+            Text(
+              msg,
+              style: const TextStyle(color: Colors.deepPurpleAccent, fontSize: 16),
+            ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          Navigator.of(context)
-              .push(MaterialPageRoute(builder: (BuildContext context) {
-            return const StriveHomePage(title: 'Strive Home');
-          }));
+          showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return AlertDialog(
+                  scrollable: true,
+                  title: const Text('Send Message'),
+                  content: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Form(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          TextFormField(
+                              decoration: const InputDecoration(
+                                labelText: 'ID (1-5)',
+                                icon: Icon(Icons.account_box),
+                              ),
+                              onChanged: (String? newID) {
+                                sendID = newID!;
+                              }),
+                          TextFormField(
+                              decoration: const InputDecoration(
+                                labelText: 'Message',
+                                icon: Icon(Icons.abc),
+                              ),
+                              onChanged: (String? newMsg) {
+                                sendMsg = newMsg!;
+                              }),
+                        ],
+                      ),
+                    ),
+                  ),
+                  actions: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          ElevatedButton(
+                              child: const Text("Cancel"),
+                              onPressed: () {
+                                Navigator.pop(context, 'Cancel');
+                              }),
+                          const SizedBox(width: 50),
+                          ElevatedButton(
+                              child: const Text("Submit"),
+                              onPressed: () {
+                                sendMessage(sendID, sendMsg);
+                                setState(() {});
+                                Navigator.pop(context, 'Cancel');
+                              })
+                        ],
+                      ),
+                    ),
+                  ],
+                );
+              });
         },
-        child: const Icon(Icons.account_tree),
+        child: const Icon(Icons.add),
       ),
     );
   }
