ece01dae016339cc1f120e67df01d3534de36a01
Brock Krusemark <kruse331@morris.umn.edu>
2023-02-13 00:55:40 -0600

    weight_data page now uses StreamBuilder to create dynamic list of widgets from each user's firestore collection data! StreamBuilder is also a listener and automatically adjusts on each entry added

--- lib/weight_data.dart
+++ lib/weight_data.dart
@@ -1,9 +1,85 @@
+import 'dart:core';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class weightData extends StatelessWidget {
-  const weightData({super.key});
+String userName = '';
+class WeightDataPage extends StatefulWidget {
+  WeightDataPage(String id, {super.key}) {
+     userName = id;
+  }
+  @override
+  State<WeightDataPage> createState() => _WeightDataPageState();
+}
+
+class _WeightDataPageState extends State<WeightDataPage> {
+  final databaseWeightRef = FirebaseFirestore.instance.collection('Users');
+  late Stream<QuerySnapshot> _stream;
+  String date='',weight='',bodyFat='';
+
+  void uploadEntry(String newDate, String newWeight, String newBodyFat) async {
+    databaseWeightRef.doc(userName).collection('Weight').add({'Date': newDate, 'Weight': newWeight, 'bodyFat': newBodyFat});
+  }
+
+  void _showAddDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Add Weight Data'),
+          content: Form(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                TextFormField(
+                  decoration: const InputDecoration(labelText: 'Date'),
+                  onChanged: (String? newDate) {
+                    date = newDate!;
+                  },
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(labelText: 'Weight'),
+                  onChanged: (String? newWeight) {
+                    weight = newWeight!;
+                  },
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(labelText: 'Body Fat Percentage'),
+                  onChanged: (String? newBodyFat) {
+                    bodyFat = newBodyFat!;
+                  },
+                ),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            MaterialButton(
+              child: const Text('Cancel'),
+              onPressed: () {
+                Navigator.pop(context, 'Cancel');
+              },
+            ),
+            MaterialButton(
+              child: const Text('Submit'),
+              onPressed: () {
+                if (date!=''&&weight!='') {
+                  setState(() {
+                    uploadEntry(date, weight, bodyFat);
+                  });
+                  Navigator.pop(context, 'Cancel');
+                }
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _stream = databaseWeightRef.doc(userName).collection('Weight').snapshots();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,29 +88,28 @@ class weightData extends StatelessWidget {
         title: const Text("Weight Page"),
       ),
       body: StreamBuilder(
-        stream: FirebaseFirestore.instance.collection('history').snapshots(),
-        builder: (context, snapshot) {
-          if (!snapshot.hasData) {
-            return const Center(
-              child: CircularProgressIndicator(color: Colors.cyanAccent,),
-            );
+        stream: _stream,
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError) {
+            return Text('Error: ${snapshot.error}');
           }
-
-          final int? historyCount = snapshot.data?.docs.length;
-          return ListView.builder(
-            itemCount: historyCount,
-            itemBuilder: (context, index) {
-              final DocumentSnapshot history =
-              snapshot.data?.docs[index] as DocumentSnapshot<Object?>;
-              return ListTile(
-                title: Text("Date: ${history['date']}", style: const TextStyle(color: Colors.white),),
-                subtitle: Text("Rating: ${history['rating']}", style: const TextStyle(color: Colors.white70),),
-              );
-            },
+          switch (snapshot.connectionState) {
+            case ConnectionState.waiting:
+              return const Center(child: CircularProgressIndicator());
+            default:
+              return ListView(
+                children: List<Widget>.from(snapshot.data?.docs.map((DocumentSnapshot document) {
+                  return ListTile(
+                    title: Text(document.get('Date').toString()),
+                    subtitle: Text(document.get('Weight').toString()),
+                  );
+                })?? []),
           );
+          }
         },
       ),
-      floatingActionButton: FloatingActionButton(onPressed: () {},
+      floatingActionButton: FloatingActionButton(onPressed: _showAddDialog,
+        child: const Icon(Icons.add),
       ),
     );
   }
