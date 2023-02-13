89ecb5291f6f2252501f297fe2451a37687bef8b
kruse331 <kruse331@morris.umn.edu>
2023-02-13 09:49:07 -0600

    rename weight_data.dart -> weight_page.dart

--- lib/profile_page.dart
+++ lib/profile_page.dart
@@ -2,7 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
-import 'package:strive/weight_data.dart';
+import 'package:strive/weight_page.dart';
 
 import 'messages.dart';
 
