e0bbb2a2b52e94c7dfc2ba6de7bcfcf970729b60
Brock Krusemark <kruse331@morris.umn.edu>
2023-02-19 03:30:20 -0600

    add bottom nav bar - change app bar

--- lib/weight_page.dart
+++ lib/weight_page.dart
@@ -2,6 +2,12 @@ import 'dart:core';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:strive/add_weight_page.dart';
+import 'package:strive/community_page.dart';
+import 'package:strive/food_page.dart';
+import 'package:strive/messages_page.dart';
+import 'package:strive/profile_page.dart';
+import 'package:strive/strive_colors.dart';
+import 'package:strive/workout_page.dart';
 
 String userName = '';
 
@@ -35,37 +41,145 @@ class _WeightPageState extends State<WeightPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Weight Page"),
+        toolbarHeight: 100,
+        flexibleSpace: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            IconButton(
+              icon: Image.asset("assets/strive_logo.png"),
+              iconSize: 140,
+              onPressed: () {
+                Navigator.of(context)
+                    .push(MaterialPageRoute(builder: (BuildContext context) {
+                  return CommunityPage(userName);
+                }));
+              },
+            ),
+          ],
+        ),
       ),
-      body: StreamBuilder(
-        stream: _stream,
-        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-          if (snapshot.hasError) {
-            return Text('Error: ${snapshot.error}');
-          }
-          switch (snapshot.connectionState) {
-            case ConnectionState.waiting:
-              return const Center(child: CircularProgressIndicator());
-            default:
-              return ListView(
-                children: List<Widget>.from(
-                    snapshot.data?.docs.map((DocumentSnapshot document) {
-                          return ListTile(
-                            title: Text(document.get('Date').toString()),
-                            subtitle: Text("${document.get('Weight')}lbs;   ${document.get('BodyFat')}%", style: const TextStyle(color: Colors.deepPurple, fontSize: 18),),
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/strive_background.png"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Column(
+          children: [
+            Flexible(
+              flex: 9,
+              fit: FlexFit.tight,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  const Spacer(flex: 1),
+                  Text(userName,
+                      style: const TextStyle(
+                          color: Colors.deepPurpleAccent, fontSize: 28)),
+                  const Spacer(flex: 1),
+                  StreamBuilder(
+                    stream: _stream,
+                    builder: (BuildContext context,
+                        AsyncSnapshot<QuerySnapshot> snapshot) {
+                      if (snapshot.hasError) {
+                        return Text('Error: ${snapshot.error}');
+                      }
+                      switch (snapshot.connectionState) {
+                        case ConnectionState.waiting:
+                          return const Center(
+                              child: CircularProgressIndicator());
+                        default:
+                          return ListView(
+                            children: List<Widget>.from(snapshot.data?.docs
+                                    .map((DocumentSnapshot document) {
+                                  return ListTile(
+                                    title:
+                                        Text(document.get('Date').toString()),
+                                    subtitle: Text(
+                                      "${document.get('Weight')}lbs;   ${document.get('BodyFat')}%",
+                                      style: const TextStyle(
+                                          color: Colors.deepPurple,
+                                          fontSize: 18),
+                                    ),
+                                  );
+                                }) ??
+                                []),
                           );
-                        }) ??
-                        []),
-              );
-          }
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {Navigator.of(context)
-            .push(MaterialPageRoute(builder: (BuildContext context) {
-          return AddWeightPage(userName);
-        }));},
-        child: const Icon(Icons.add),
+                      }
+                    },
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.of(context).push(
+                          MaterialPageRoute(builder: (BuildContext context) {
+                        return AddWeightPage(userName);
+                      }));
+                    },
+                    child: const Text('Add Workout Data'),
+                  ),
+                ],
+              ),
+            ),
+            Flexible(
+              flex: 1,
+              fit: FlexFit.tight,
+              child: Container(
+                decoration: const BoxDecoration(color: strive_lavender),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    IconButton(
+                      icon: Image.asset("assets/star_icon_dark.png"),
+                      onPressed: () {
+                        Navigator.of(context).push(
+                            MaterialPageRoute(builder: (BuildContext context) {
+                          return ProfilePage(userName);
+                        }));
+                      },
+                    ),
+                    const Spacer(flex: 1),
+                    IconButton(
+                      icon: Image.asset("assets/apple_icon_dark.png"),
+                      onPressed: () {
+                        Navigator.of(context).push(
+                            MaterialPageRoute(builder: (BuildContext context) {
+                          return FoodPage(userName);
+                        }));
+                      },
+                    ),
+                    const Spacer(flex: 1),
+                    IconButton(
+                      icon: Image.asset("assets/weights_icon_dark.png"),
+                      onPressed: () {
+                        Navigator.of(context).push(
+                            MaterialPageRoute(builder: (BuildContext context) {
+                          return WorkoutPage(userName);
+                        }));
+                        },
+                    ),
+                    const Spacer(flex: 1),
+                    IconButton(
+                      icon: Image.asset("assets/scale_icon_light.png"),
+                      onPressed: () {},
+                    ),
+                    const Spacer(flex: 1),
+                    IconButton(
+                      icon: Image.asset("assets/message_icon_dark.png"),
+                      onPressed: () {
+                        Navigator.of(context).push(
+                            MaterialPageRoute(builder: (BuildContext context) {
+                          return MessagePage(userName);
+                        }));
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
