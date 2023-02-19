49add96f78d3e97b4cf3ee55365465998dde89af
Brock Krusemark <kruse331@morris.umn.edu>
2023-02-19 00:07:07 -0600

    add purp

--- lib/strive_colors.dart
+++ lib/strive_colors.dart
@@ -3,3 +3,4 @@ import 'package:flutter/material.dart';
 const strive_lavender = Color.fromRGBO(226, 225, 240, 1);
 const strive_cyan = Color.fromRGBO(42, 172, 226, 1);
 const strive_navy = Color.fromRGBO(9, 33, 63, 1);
+const strive_purple = Color.fromRGBO(152, 37, 140, 1);
