e714ead61f1bc6f1d736a330aa5ff4e9b9e8b1f0
Brock Krusemark <kruse331@morris.umn.edu>
2023-02-19 02:31:45 -0600

    Added bottom navigation bar - changed app bar

---
+++ lib/food_page.dart
@@ -0,0 +1,142 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:strive/profile_page.dart';
+import 'package:strive/weight_page.dart';
+import 'package:strive/workout_page.dart';
+import 'package:strive/messages_page.dart';
+import 'package:strive/strive_colors.dart';
+import 'package:strive/community_page.dart';
+
+final databaseUserRef = FirebaseFirestore.instance.collection('Users');
+String userName = '';
+
+class FoodPage extends StatefulWidget {
+  FoodPage(String id, {super.key}) {
+    userName = id;
+  }
+
+  @override
+  State<FoodPage> createState() => _FoodPage();
+}
+
+class _FoodPage extends State<FoodPage> {
+  String user = '';
+
+  Future<DocumentSnapshot> retrieveProfileData(String id) async {
+    return databaseUserRef.doc(id).get();
+  }
+
+  Future<void> getUserData() async {}
+
+  _FoodPage() {
+    getUserData();
+    Future.delayed(const Duration(milliseconds: 1000), () {
+      setState(() {});
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 100,
+        flexibleSpace: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            IconButton(
+              icon: Image.asset("assets/strive_logo.png"),
+              iconSize: 140,
+              onPressed: () {
+                Navigator.of(context)
+                    .push(MaterialPageRoute(builder: (BuildContext context) {
+                  return CommunityPage(userName);
+                }));
+              },
+            ),
+          ],
+        ),
+      ),
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/strive_background.png"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Column(
+          children: [
+            Flexible(
+              flex: 9,
+              fit: FlexFit.tight,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  const Spacer(flex: 1),
+                  Text(userName,
+                      style: const TextStyle(
+                          color: Colors.deepPurpleAccent, fontSize: 28)),
+                  const Spacer(flex: 1),
+                ],
+              ),
+            ),
+            Flexible(
+              flex: 1,
+              fit: FlexFit.tight,
+              child: Container(
+                decoration: const BoxDecoration(color: strive_lavender),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    IconButton(
+                      icon: Image.asset("assets/star_icon_dark.png"),
+                      onPressed: () {Navigator.of(context).push(
+                            MaterialPageRoute(builder: (BuildContext context) {
+                          return ProfilePage(userName);
+                        }));},
+                    ),
+                    const Spacer(flex: 1),
+                    IconButton(
+                      icon: Image.asset("assets/apple_icon_light.png"),
+                      onPressed: () {},
+                    ),
+                    const Spacer(flex: 1),
+                    IconButton(
+                      icon: Image.asset("assets/weights_icon_dark.png"),
+                      onPressed: () {
+                        Navigator.of(context).push(
+                            MaterialPageRoute(builder: (BuildContext context) {
+                          return WorkoutPage(userName);
+                        }));
+                      },
+                    ),
+                    const Spacer(flex: 1),
+                    IconButton(
+                      icon: Image.asset("assets/scale_icon_dark.png"),
+                      onPressed: () {
+                        Navigator.of(context).push(
+                            MaterialPageRoute(builder: (BuildContext context) {
+                          return WeightPage(userName);
+                        }));
+                      },
+                    ),
+                    const Spacer(flex: 1),
+                    IconButton(
+                      icon: Image.asset("assets/message_icon_dark.png"),
+                      onPressed: () {
+                        Navigator.of(context).push(
+                            MaterialPageRoute(builder: (BuildContext context) {
+                          return MessagePage(userName);
+                        }));
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
