f642e68abfd3531795367fe4c4f14a819266f19d
kruse331 <kruse331@morris.umn.edu>
2023-02-24 16:46:51 -0600

    Progress on user settings implementation

--- lib/user_settings_page.dart
+++ lib/user_settings_page.dart
@@ -16,19 +16,33 @@ class UserSettingsPage extends StatefulWidget {
 }
 
 class _UserSettingsPageState extends State<UserSettingsPage> {
-  String user = '';
+  bool visible = false;
+  String units = 'Imperial';
 
-  Future<DocumentSnapshot> retrieveProfileData(String id) async {
-    return databaseUserRef.doc(id).get();
+  Future<DocumentSnapshot> retrieveSettingsSnapshot(String id) async {
+    return databaseUserRef
+        .doc(id)
+        .collection('Settings')
+        .doc('userSettings')
+        .get();
   }
 
-  Future<void> getUserData() async {}
+  Future<void> getUserData() async {
+    final DocumentSnapshot settingsSnapshot =
+        await retrieveSettingsSnapshot(userName);
+    visible = settingsSnapshot.get('Visible');
+  }
 
   _UserSettingsPageState() {
     getUserData();
     Future.delayed(const Duration(milliseconds: 1000), () {
       setState(() {});
     });
+  }
+
+  @override
+  void initState() {
+    super.initState();
   }
 
   @override
@@ -66,18 +80,136 @@ class _UserSettingsPageState extends State<UserSettingsPage> {
             mainAxisAlignment: MainAxisAlignment.start,
             children: <Widget>[
               Flexible(
-                flex: 1,
-                child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                mainAxisSize: MainAxisSize.max,
-                children: const [
-                  Text('Settings', style: TextStyle(color: strive_lavender, fontSize: 18),),
-                ],
-              )),
+                  flex: 1,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.max,
+                    children: const [
+                      Text(
+                        'Settings',
+                        style: TextStyle(color: strive_lavender, fontSize: 18),
+                      ),
+                    ],
+                  )),
               const Spacer(flex: 1),
-              Text(userName,
-                  style: const TextStyle(
-                      color: Colors.deepPurpleAccent, fontSize: 28)),
+              Container(
+                color: strive_navy,
+                child: Flexible(
+                  flex: 2,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      Text(
+                        userName,
+                        style:
+                            const TextStyle(color: strive_purple, fontSize: 16),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              const Spacer(flex: 1),
+              Container(
+                color: strive_navy,
+                child: Flexible(
+                  flex: 2,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      const Spacer(flex: 1),
+                      const Text(
+                        "Visible:",
+                        style: TextStyle(color: strive_purple, fontSize: 16),
+                        textAlign: TextAlign.center,
+                      ),
+                      const Spacer(flex: 3),
+                      Text(
+                        visible.toString(),
+                        style:
+                            const TextStyle(color: strive_purple, fontSize: 16),
+                      ),
+                      const Spacer(flex: 1),
+                    ],
+                  ),
+                ),
+              ),
+              const Spacer(flex: 1),
+              Container(
+                color: strive_navy,
+                child: Flexible(
+                  flex: 2,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+              const Spacer(flex: 1),
+                      const Text(
+                        "Placeholder:",
+                        style: TextStyle(color: strive_purple, fontSize: 16),
+                      ),
+                      const Spacer(flex: 3),
+                      Text(
+                        visible.toString(),
+                        style:
+                            const TextStyle(color: strive_purple, fontSize: 16),
+                      ),
+              const Spacer(flex: 1),
+                    ],
+                  ),
+                ),
+              ),
+              const Spacer(flex: 1),
+              Container(
+                color: strive_navy,
+                child: Flexible(
+                  flex: 2,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+              const Spacer(flex: 1),
+                      const Text(
+                        "Placeholder:",
+                        style: TextStyle(color: strive_purple, fontSize: 16),
+                      ),
+                      const Spacer(flex: 3),
+                      Text(
+                        visible.toString(),
+                        style:
+                            const TextStyle(color: strive_purple, fontSize: 16),
+                      ),
+              const Spacer(flex: 1),
+                    ],
+                  ),
+                ),
+              ),
+              const Spacer(flex: 1),
+              Container(
+                color: strive_navy,
+                child: Flexible(
+                  flex: 2,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+              const Spacer(flex: 1),
+                      const Text(
+                        "Placeholder:",
+                        style: TextStyle(color: strive_purple, fontSize: 16),
+                      ),
+                      const Spacer(flex: 3),
+                      Text(
+                        visible.toString(),
+                        style:
+                            const TextStyle(color: strive_purple, fontSize: 16),
+                      ),
+                      const Spacer(flex: 1),
+                    ],
+                  ),
+                ),
+              ),
               const Spacer(flex: 1),
             ],
           ),
