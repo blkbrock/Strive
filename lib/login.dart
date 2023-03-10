54d44d1e1fff43c5dbc2d81df84976900fc7a8ef
Brock Krusemark <kruse331@morris.umn.edu>
2023-03-10 01:44:42 -0600

    new login page

---
+++ lib/login.dart
@@ -0,0 +1,90 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:sign_in_with_apple/sign_in_with_apple.dart';
+import 'package:strive/auth.dart';
+import 'package:strive/profile_page.dart';
+import 'package:strive/strive_styles.dart';
+import 'package:flutter/material.dart';
+
+String _userName = '';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.all(30),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            SizedBox(height: MediaQuery.of(context).size.height * 0.3, 
+            child: Image.asset('assets/strive_logo.png')),
+            LoginButton(
+              text: 'Sign in with Email',
+              icon: FontAwesomeIcons.envelope,
+              color: Colors.blue,
+              loginMethod: AuthService().emailLogin,
+            ),
+            LoginButton(
+              text: 'Sign in with Google',
+              icon: FontAwesomeIcons.google,
+              color: Colors.blue,
+              loginMethod: AuthService().googleLogin,
+            ),
+            FutureBuilder<Object>(
+              future: SignInWithApple.isAvailable(),
+              builder: (context, snapshot) {
+                if (snapshot.data == true) {
+                  return SignInWithAppleButton(
+                    onPressed: () {
+                      AuthService().signInWithApple();
+                    },
+                  );
+                } else {
+                  return Container();
+                }
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class LoginButton extends StatelessWidget {
+  final Color color;
+  final IconData icon;
+  final String text;
+  final Function loginMethod;
+
+  const LoginButton(
+      {super.key,
+      required this.text,
+      required this.icon,
+      required this.color,
+      required this.loginMethod});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(bottom: 10),
+      child: ElevatedButton.icon(
+        icon: Icon(
+          icon,
+          color: Colors.white,
+          size: 20,
+        ),
+        style: TextButton.styleFrom(
+          padding: const EdgeInsets.all(24),
+          backgroundColor: color,
+        ),
+        onPressed: () => loginMethod(),
+        label: Text(text, textAlign: TextAlign.center),
+      ),
+    );
+  }
+}
